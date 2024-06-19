72c8dfdb07ce5dbefdc52514b76d58b203b81c2b
Saludo <alexis.j.saludo@hotmail.fr>
2024-06-19 22:01:43 +0200

    add Property image to RequestDTO

--- src/main/kotlin/fr/zakaoai/coldlibrarybackend/enums/RequestType.kt
+++ src/main/kotlin/fr/zakaoai/coldlibrarybackend/enums/RequestType.kt
@@ -2,6 +2,5 @@ package fr.zakaoai.coldlibrarybackend.enums
 
 enum class RequestType(val type: String) {
     MOVE_TO_SERVER("MOVE_TO_SERVER"),
-    ADD_TO_SERVER("MOVE_TO_SERVER")
-
+    ADD_TO_SERVER("ADD_TO_SERVER")
 }
--- src/main/kotlin/fr/zakaoai/coldlibrarybackend/infrastructure/db/services/RequestRepository.kt
+++ src/main/kotlin/fr/zakaoai/coldlibrarybackend/infrastructure/db/services/RequestRepository.kt
@@ -12,7 +12,7 @@ interface RequestRepository : ReactiveCrudRepository<Request, Long> {
 
     companion object {
         const val getWithInformation: String =
-            "SELECT r.*, creator.name as \"creator\", assignedUser.name as \"assigned_user\", anime.title as \"anime_title\"  FROM cold_library.\"Request\" r INNER JOIN cold_library.\"User\" creator ON r.user_id = creator.user_id LEFT JOIN cold_library.\"User\" assignedUser ON r.assigned_user_id = assignedUser.user_id INNER JOIN cold_library.\"Anime\" anime on r.mal_id = anime.mal_id"
+            "SELECT r.*, creator.name as \"creator\", assignedUser.name as \"assigned_user\", anime.title as \"anime_title\", anime.mal_img as \"mal_img\"  FROM cold_library.\"Request\" r INNER JOIN cold_library.\"User\" creator ON r.user_id = creator.user_id LEFT JOIN cold_library.\"User\" assignedUser ON r.assigned_user_id = assignedUser.user_id INNER JOIN cold_library.\"Anime\" anime on r.mal_id = anime.mal_id"
     }
 
     @Query("$getWithInformation WHERE r.user_id = :creatorId")
